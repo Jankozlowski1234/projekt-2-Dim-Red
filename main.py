c77101a354bd9205d8ac541ab52d117d07e66eb2
Jankozlowski1234 <jaa.kozaa@gmail.com>
2024-05-28 10:39:12 +0200

    Reading the data

--- main.py
+++ main.py
@@ -1,1 +1,41 @@
-print("https://www.geeksforgeeks.org/hidden-markov-model-in-machine-learning/")
+import argparse
+import pandas as pd
+import numpy as np
+import math
+
+import os
+from os.path import isfile, join
+
+
+###parser part
+parser = argparse.ArgumentParser()
+parser.add_argument("--train", help="name of file of data to be train of")
+parser.add_argument("--test", help="name of folder of tests files")
+parser.add_argument("--output", help="name of file to be resault be saved")
+args = parser.parse_args()
+
+
+if args.train:
+    TRAIN_NAME = args.train
+else:
+    TRAIN_NAME = "house3_5devices_train.csv"
+
+if args.test:
+    TEST_NAME = args.test
+else:
+    TEST_NAME = "test_folder"
+
+if args.output:
+    RESULT_NAME = args.output
+else:
+    RESULT_NAME = "results.txt"
+
+
+#reading data
+tests = []
+for filename in os.listdir(TEST_NAME):
+    df = pd.read_csv(os.path.join(os.getcwd(),TEST_NAME ,filename))
+    tests.append(df.iloc[:, 1])
+nr_of_tests = len(tests)
+
+train = pd.read_csv(TRAIN_NAME).iloc[:,1:]
